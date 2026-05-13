a27a6650b54f206b99e18bdf8dcabfd451519f5c
IT25100330 <it25100330@my.sliit.lk>
2026-05-13 15:41:05 +0530

    Add userDTO
    I create this DTO to enhance security otherwise controller directly connect with entity so that not good for our system.

--- RuSiri/src/main/java/com.hotel/RuSiri/DTO/User/ChangePasswordDTO.java
+++ RuSiri/src/main/java/com.hotel/RuSiri/DTO/User/ChangePasswordDTO.java
@@ -1,1 +1,11 @@
 
+package com.hotel.RuSiri.DTO.User;
+
+import lombok.Data;
+
+@Data
+public class ChangePasswordDTO {
+
+    private String currentPassword;
+    private String newPassword;
+}
--- RuSiri/src/main/java/com.hotel/RuSiri/DTO/User/MyProfileDTO.java
+++ RuSiri/src/main/java/com.hotel/RuSiri/DTO/User/MyProfileDTO.java
@@ -1,1 +1,18 @@
 
+package com.hotel.RuSiri.DTO.User;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MyProfileDTO {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String phoneNumber; // ✅
+    private String nic;         // ✅
+    private String firstName;
+    private String lastName;
+}
